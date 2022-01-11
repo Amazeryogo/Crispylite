42844481fce3a2518d8a369b828e7630787cc952
Suvid Datta <suvid.datta@gmail.com>
2022-01-11 08:13:44 +0530

    some more changes

--- main.c
+++ main.c
@@ -1,40 +1,5 @@
 /* so basically a memory only database */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <unistd.h>
-
-/* store the data in memory */
-struct data {
-    char *key;
-    char *value;
-    struct data *next;
-};
-
-int store(char *key, char *value, struct data **head){
-    struct data *new_node = malloc(sizeof(struct data));
-    if(new_node == NULL){
-        return -1;
-    }
-    new_node->key = key;
-    new_node->value = value;
-    new_node->next = *head;
-    *head = new_node;
-    return 0;
-}
-
-int retrieve(char *key, struct data *head, char **value){
-    struct data *current = head;
-    while(current != NULL){
-        if(strcmp(current->key, key) == 0){
-            *value = current->value;
-            return 0;
-        }
-        current = current->next;
-    }
-    return -1;
-}
+#include "main.h"
 
 int main(){
     /* ask for a input, if the first word is store, then store the key and value */
